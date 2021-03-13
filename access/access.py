7ef0176d5ddc44a56dfa8437c44efbf812d61264
Kreusada <kreusadaprojects@gmail.com>
2021-03-13 19:02:12 +0000

    [Access] Add license

--- access/access.py
+++ access/access.py
@@ -1,7 +1,32 @@
+"""
+MIT License
+
+Copyright (c) 2021 kreusada
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 import discord
 
 from redbot.core import commands
 from redbot.core.utils.chat_formatting import box
+
 
 class Access(commands.Cog):
     """
@@ -12,6 +37,7 @@ class Access(commands.Cog):
         self.bot = bot
 
     async def send_access(self, ctx, member: discord.Member, access_type: str):
+        
         if access_type == 'text':
             build = ctx.guild.text_channels
             prefix = '#'
